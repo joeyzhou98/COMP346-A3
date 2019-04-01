b0338e14e88614b0fbb655237beadb697ac55e4b
Weiran Zhao <34318341+VeraZwr@users.noreply.github.com>
2019-03-31 21:38:37 -0400

    Add files via upload

---
+++ src/src/DiningPhilosophers.java
@@ -0,0 +1,125 @@
+
+/**
+ * Class DiningPhilosophers
+ * The main starter.
+ *
+ * @author Serguei A. Mokhov, mokhov@cs.concordia.ca
+ */
+public class DiningPhilosophers
+{
+	/*
+	 * ------------
+	 * Data members
+	 * ------------
+	 */
+
+	/**
+	 * This default may be overridden from the command line
+	 */
+	public static final int DEFAULT_NUMBER_OF_PHILOSOPHERS = 4;
+
+	/**
+	 * Dining "iterations" per philosopher thread
+	 * while they are socializing there
+	 */
+	public static final int DINING_STEPS = 10;
+
+	/**
+	 * Our shared monitor for the philosphers to consult
+	 */
+	public static Monitor soMonitor = null;
+
+	/*
+	 * -------
+	 * Methods
+	 * -------
+	 */
+
+	/**
+	 * Main system starts up right here
+	 */
+	public static void main(String[] argv)
+	{
+		//System.out.println(argv[0]);
+		try
+		{
+			/*
+			 * TODO:
+			 * Should be settable from the command line
+			 * or the default if no arguments supplied.
+			 */
+			int iPhilosophers = DEFAULT_NUMBER_OF_PHILOSOPHERS; //default
+			if (argv.length == 1) //if there is an argument
+			{
+				try
+				{
+					int argument = Integer.parseInt(argv[0]); //parse argument
+					if (argument < 1) //if it is non-positive
+					{
+						//print error message and exit
+						System.out.println(argv[0] + " is not a positive decimal integer");
+						System.out.println("Usage: java DiningPhilosophers [NUMBER_OF_PHILOSOPHERS]");
+						System.exit(-1);
+					}
+					else
+					{
+						iPhilosophers = argument; //if it is positive integer then set iPhilosophers to the passed integer
+					}
+				}
+				catch (NumberFormatException e)
+				{
+					//print error message and exit
+					System.out.println(argv[0] + " is not a positive decimal integer");
+					System.out.println("Usage: java DiningPhilosophers [NUMBER_OF_PHILOSOPHERS]");
+					System.exit(-1);
+				}
+			}
+
+			// Make the monitor aware of how many philosophers there are
+			soMonitor = new Monitor(iPhilosophers);
+
+			// Space for all the philosophers
+			Philosopher aoPhilosophers[] = new Philosopher[iPhilosophers];
+
+			// Let 'em sit down
+			for(int j = 0; j < iPhilosophers; j++)
+			{
+				aoPhilosophers[j] = new Philosopher();
+				aoPhilosophers[j].start();
+			}
+
+			System.out.println
+			(
+				iPhilosophers +
+				" philosopher(s) came in for a dinner."
+			);
+
+			// Main waits for all its children to die...
+			// I mean, philosophers to finish their dinner.
+			for(int j = 0; j < iPhilosophers; j++)
+				aoPhilosophers[j].join();
+
+			System.out.println("All philosophers have left. System terminates normally.");
+		}
+		catch(InterruptedException e)
+		{
+			System.err.println("main():");
+			reportException(e);
+			System.exit(1);
+		}
+	} // main()
+
+	/**
+	 * Outputs exception information to STDERR
+	 * @param poException Exception object to dump to STDERR
+	 */
+	public static void reportException(Exception poException)
+	{
+		System.err.println("Caught exception : " + poException.getClass().getName());
+		System.err.println("Message          : " + poException.getMessage());
+		System.err.println("Stack Trace      : ");
+		poException.printStackTrace(System.err);
+	}
+}
+
+// EOF
